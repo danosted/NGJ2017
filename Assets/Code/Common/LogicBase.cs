c4ec352fc852cdbcd2ce9a501fc738651d74aa75
danosted <danosted@gmail.com>
2017-04-22 10:46:41 +0200

    initial

---
+++ Assets/Code/Common/LogicBase.cs
@@ -0,0 +1,24 @@
+﻿namespace Assets.Code.Common
+{
+    using DataAccess;
+    using IoC;
+    using MonoBehaviours.Configuration;
+
+    public class LogicBase
+    {
+        #region Properties
+        protected IoC Container { get; set; }
+        protected PrefabManager PrefabManager { get; private set; }
+        protected GlobalConfiguration Configuration { get; private set; }
+        #endregion
+        
+        #region Constructors
+        public LogicBase(IoC container, PrefabManager prefabManager, GlobalConfiguration config)
+        {
+            Container = container;
+            PrefabManager = prefabManager;
+            Configuration = config;
+        }
+        #endregion
+    }
+}
---
+++ Assets/Code/GameLogic/FlowLogic.cs
@@ -0,0 +1,50 @@
+﻿namespace Assets.Code.GameLogic
+{
+    using MonoBehaviours.Configuration;
+    using Common;
+    using DataAccess;
+    using DataAccess.DTOs;
+    using IoC;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+    public class FlowLogic : LogicBase
+    {
+
+        public FlowLogic(IoC container, PrefabManager prefabManager, GlobalConfiguration config) : base(container, prefabManager, config)
+        {
+        }
+
+        public void InitializeGame()
+        {
+            // Initialize UI
+            Container.Resolve<UserInterfaceLogic>().InitializeGameMenuCanvas();
+
+            // Initialize Audio
+            Container.Resolve<AudioLogic>().InitializeAudio();
+
+        }
+
+        public void StartGame()
+        {
+            // Change to game UI
+            Container.Resolve<UserInterfaceLogic>().InitializeGameCanvas();
+            
+            // Create an object
+            var obj = PrefabManager.GetPrefab(Configuration.prefab_moveable_object);
+            obj.Activate(Container, Vector3.zero);
+        }
+
+        public void GameOver()
+        {
+            Container.Resolve<UserInterfaceLogic>().InitializeGameOverCanvas();
+            PrefabManager.Shutdown();
+        }
+
+        public void RestartGame()
+        {
+            // TODO 2 (DRO): this could be done more efficiently
+            SceneManager.LoadScene(0);
+        }
+    }
+}
