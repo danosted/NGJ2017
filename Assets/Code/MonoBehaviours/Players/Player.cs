15ec572118b1fbcde62abf3ec35bf7ec9faf3079
danosted <danosted@gmail.com>
2017-04-22 15:06:42 +0200

    movement changed

--- Assets/Code/MonoBehaviours/Players/Player.cs
+++ Assets/Code/MonoBehaviours/Players/Player.cs
@@ -12,7 +12,7 @@
         protected float JumpMagnitude { get; set; }
         protected float MoveMagnitude { get; set; }
         private Vector3 _jumpDirection;
-
+        protected Rigidbody RigidBody { get; set; }
 
         public virtual void Activate(IoC container)
         {
@@ -25,6 +25,8 @@
             // Spawn player inside the level
             var collider = GetComponent<Collider>();
             transform.position = transform.position + Vector3.forward * collider.bounds.extents.x;
+
+            RigidBody = GetComponent<Rigidbody>();
         }
 
         public void Deactivate()
@@ -32,20 +34,20 @@
             PrefabManager.ReturnPrefab(this);
         }
 
-        protected virtual void Update()
+        protected virtual void FixedUpdate()
         {
             if (Input.GetButtonUp("Jump") && !IsJumping())
             {
                 Jump();
             }
             Move();
+            //MoveRigid();
             //CheckOutOfBounds();
         }
 
         private bool IsJumping()
         {
             var rb = GetComponent<Rigidbody>();
-            Debug.LogFormat("y velocity: {0}.", rb.velocity.y);
             if (rb.velocity.y < -0.01f || 0.01f < rb.velocity.y)
             {
                 return true;
@@ -55,7 +57,7 @@
 
         protected virtual void Jump()
         {
-            GetComponent<Rigidbody>().AddForce(_jumpDirection * JumpMagnitude, ForceMode.Impulse);
+            RigidBody.AddForce(_jumpDirection * JumpMagnitude, ForceMode.Impulse);
             //transform.Translate(_jumpDirection.normalized * Time.deltaTime * Speed);
         }
 
@@ -65,6 +67,14 @@
             var zMove = Input.GetAxis("Vertical");
             //var jump = Input.
             transform.Translate(new Vector3(Time.deltaTime * xMove * MoveMagnitude, 0f, Time.deltaTime * zMove * MoveMagnitude));
+        }
+
+        protected virtual void MoveRigid()
+        {
+            var xMove = Input.GetAxis("Horizontal");
+            var zMove = Input.GetAxis("Vertical");
+            Debug.LogFormat("mag {0}.", Time.deltaTime * MoveMagnitude);
+            RigidBody.AddForce(new Vector3(xMove, 0f, zMove).normalized * Time.deltaTime * MoveMagnitude, ForceMode.VelocityChange);
         }
 
         protected virtual void CheckOutOfBounds()
